93ebaea96265f6b80382ac5f8ceb3d5dfd6e54ad
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-29 00:08:00 +0100

    removes duplication from tests

--- src/test/java/uk/co/mruoc/gildedrose/GildedRoseTest.java
+++ src/test/java/uk/co/mruoc/gildedrose/GildedRoseTest.java
@@ -2,6 +2,8 @@ package uk.co.mruoc.gildedrose;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.IntStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class GildedRoseTest {
@@ -11,33 +13,28 @@ class GildedRoseTest {
         Item item = new Item("normal-item", 0, 0);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.quality).isZero();
     }
 
     @Test
-    void qualityOfNormalItemShouldDegradeTwiceAsFastAfterSellByDate() {
-        Item item = new Item("normal-item", 1, 10);
+    void qualityOfNormalItemShouldDegradeByTwoAfterSellByDate() {
+        Item item = new Item("normal-item", 0, 10);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
-
-        assertThat(item.sellIn).isZero();
-        assertThat(item.quality).isEqualTo(9);
-
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(-1);
-        assertThat(item.quality).isEqualTo(7);
+        assertThat(item.quality).isEqualTo(8);
     }
 
     @Test
-    void qualityOfAgedBrieShouldIncreaseTheOlderItGets() {
+    void qualityOfAgedBrieShouldIncreaseByTwoTheOlderItGets() {
         Item item = new Item("Aged Brie", 0, 0);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(2);
@@ -48,14 +45,9 @@ class GildedRoseTest {
         Item item = new Item("Aged Brie", 0, 50);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(-1);
-        assertThat(item.quality).isEqualTo(50);
-
-        app.updateQuality();
-
-        assertThat(item.sellIn).isEqualTo(-2);
         assertThat(item.quality).isEqualTo(50);
     }
 
@@ -64,11 +56,7 @@ class GildedRoseTest {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 0);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
+        executeNTimes(5, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(10);
         assertThat(item.quality).isEqualTo(5);
@@ -79,11 +67,7 @@ class GildedRoseTest {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 0);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
+        executeNTimes(5, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(5);
         assertThat(item.quality).isEqualTo(10);
@@ -94,11 +78,7 @@ class GildedRoseTest {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 0);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
-        app.updateQuality();
+        executeNTimes(5, app::updateQuality);
 
         assertThat(item.sellIn).isZero();
         assertThat(item.quality).isEqualTo(15);
@@ -106,15 +86,10 @@ class GildedRoseTest {
 
     @Test
     void qualityOfBackstagePassesShouldNotIncreaseAboveFifty() {
-        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 2, 50);
+        Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 50);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
-
-        assertThat(item.sellIn).isEqualTo(1);
-        assertThat(item.quality).isEqualTo(50);
-
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.sellIn).isZero();
         assertThat(item.quality).isEqualTo(50);
@@ -125,15 +100,14 @@ class GildedRoseTest {
         Item item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10);
         GildedRose app = new GildedRose(toArray(item));
 
-        app.updateQuality();
+        executeNTimes(1, app::updateQuality);
 
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isZero();
+    }
 
-        app.updateQuality();
-
-        assertThat(item.sellIn).isEqualTo(-2);
-        assertThat(item.quality).isZero();
+    private void executeNTimes(int n, Runnable action) {
+        IntStream.range(0, n).forEach(i -> action.run());
     }
 
     private static Item[] toArray(Item item) {
