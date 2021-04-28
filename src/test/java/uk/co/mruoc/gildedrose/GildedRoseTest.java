37e985ced8929ce0e8d40be41d04ee1ed114bc7f
Michael Ruocco <michael.ruocco@hotmail.com>
2021-04-28 23:46:00 +0100

    tests quality of ages brie increases as it gets older

--- src/test/java/uk/co/mruoc/gildedrose/GildedRoseTest.java
+++ src/test/java/uk/co/mruoc/gildedrose/GildedRoseTest.java
@@ -7,7 +7,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class GildedRoseTest {
 
     @Test
-    void qualityOfANormalItemShouldNeverBeNegative() {
+    void qualityOfNormalItemShouldNeverBeNegative() {
         Item item = new Item("normal-item", 0, 0);
         GildedRose app = new GildedRose(toArray(item));
 
@@ -17,7 +17,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void qualityOfANormalShouldDegradeTwiceAsFastAfterSellByDate() {
+    void qualityOfNormalItemShouldDegradeTwiceAsFastAfterSellByDate() {
         Item item = new Item("normal-item", 1, 10);
         GildedRose app = new GildedRose(toArray(item));
 
@@ -30,6 +30,17 @@ class GildedRoseTest {
 
         assertThat(item.sellIn).isEqualTo(-1);
         assertThat(item.quality).isEqualTo(7);
+    }
+
+    @Test
+    void qualityOfAgedBrieShouldIncreaseTheOlderItGets() {
+        Item item = new Item("Aged Brie", 0, 0);
+        GildedRose app = new GildedRose(toArray(item));
+
+        app.updateQuality();
+
+        assertThat(item.sellIn).isEqualTo(-1);
+        assertThat(item.quality).isEqualTo(2);
     }
 
     private static Item[] toArray(Item item) {
